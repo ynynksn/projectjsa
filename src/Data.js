fe77c88c6977dd9f5a32aab20db9537d78411792
ynynksn <trieuhungdat@gmail.com>
2023-03-08 09:52:02 +0700

    updated

--- src/Data.js
+++ src/Data.js
@@ -27,4 +27,32 @@ let shopItemsData = [
       desc: "A high-quality product with the finest material and design.",
       img: "images/aodai5.jpeg",
     },
+    {
+      id: "jfhgbvnscs",
+      name: "Cotton Velvet",
+      price: 24.99,
+      desc: "Dreamy and energetic design, suitable for a soft and styled class. ",
+      img: "images/aodai11.jpeg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Cotton Teal",
+      price: 26.99,
+      desc: "Our newest design with a very traditional and elegant touch. ",
+      img: "images/aodai15.jpeg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Cotton Black",
+      price: 19.99,
+      desc: "A product which fits everyone's budget but still highly quality.",
+      img: "images/aodai12.jpeg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Cotton Modernized Pattern",
+      price: 29.99,
+      desc: "A high-quality product with the finest material and design.",
+      img: "images/aodai5.jpeg",
+    },
   ];
